1db3cfb6068c1957e2d57c4e2684c425f9fa33e1
Soumyajit-Mukherjee <136887571+Soumyajit-Mukherjee@users.noreply.github.com>
2026-04-01 19:46:59 +0530

    Update dbconnection.java

--- src/com/hostel/dao/dbconnection.java
+++ src/com/hostel/dao/dbconnection.java
@@ -14,18 +14,6 @@ public class dbconnection {
 			String dbUrl = System.getenv("DB_URL"); 
 			String dbUser = System.getenv("DB_USER");
 			String dbPass = System.getenv("DB_PASS");
-
-			// 3. Fallback for Localhost (if cloud variables aren't found)
-			if (url == null || url.isEmpty()) {
-				// System.out.println("Connecting to Localhost...");
-				url = "jdbc:mysql://localhost:3306/hostel_management";
-				user = "root";
-				pass = "Soumyajit@123";
-			} else {
-				// System.out.println("Connecting to Cloud Database...");
-			}
-
-			// 4. Establish the Connection
 			conn = DriverManager.getConnection(dbUrl, dbUser, dbPass);
 			// System.out.println("Success: Connected to MySQL!");
 
